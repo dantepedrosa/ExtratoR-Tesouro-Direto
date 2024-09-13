20bf7627b1ace85a8335ad01c3d1a43bc7d97592
Dante Pedrosa <dantepedrosa@gmail.com>
2024-09-13 19:33:44 -0300

    Criado função de capturar lista de corretoras

--- extratorTD/__init__.py
+++ extratorTD/__init__.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 import time
 
 BOTAO_LOGIN = {
@@ -23,6 +24,8 @@ def alert_is_closed(driver):
     except:
         return True  # Alert is closed
 
+
+
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
 
@@ -34,23 +37,53 @@ driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () =>
 
 # TODO: Implementar acesso à página
 
-driver.get("https://portalinvestidor.tesourodireto.com.br/")
+def login(driver):
+        
+    driver.get("https://portalinvestidor.tesourodireto.com.br/")
 
 
-# Set up a WebDriverWait object
-wait = WebDriverWait(driver, 10) 
+    # Set up a WebDriverWait object
+    wait = WebDriverWait(driver, 10) 
 
-user_wait = WebDriverWait(driver, 60) 
+    user_wait = WebDriverWait(driver, 60) 
 
-# Execute a JavaScript command.
-driver.execute_script("alert('Faça login no Tesouro Direto!');")
+    # Execute a JavaScript command.
+    driver.execute_script("alert('Faça login no Tesouro Direto!');")
 
-# Wait for the alert to be closed
-user_wait.until(alert_is_closed)
+    # Wait for the alert to be closed
+    user_wait.until(alert_is_closed)
 
-# Wait for the new window or tab
-user_wait.until(EC.url_to_be('https://portalinvestidor.tesourodireto.com.br/MeusInvestimentos'))
+    # Wait for the new window or tab
+    user_wait.until(EC.url_to_be('https://portalinvestidor.tesourodireto.com.br/MeusInvestimentos'))
 
+def getBrokers(driver):
+
+    brokerList = {}
+
+    driver.get("https://portalinvestidor.tesourodireto.com.br/Extrato")
+
+    # Make the select element visible by modifying its style attribute
+    driver.execute_script("document.getElementById('instituicao').style.display = 'block';")
+
+    # Find the select element
+    select_element = driver.find_element(By.ID, 'instituicao')
+
+    # Wrap the element in a Select object
+    select = Select(select_element)
+
+    # Iterate over all options and print their text and values
+    for option in select.options:
+        if option.text != 'Selecione':
+            brokerList[option.get_attribute('value')] = option.text
+
+    return brokerList
+
+
+def getData(driver, brokerList):
+    pass
+
+login(driver)
+getBrokers(driver)
 
 # TODO: Implementar loop de coleta de dados
 # 
